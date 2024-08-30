372393a6c5ecb5eb4328bfe1b5347d6c47abdb1d
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-08-30 13:07:48 -0600

    Added register() function for blocks to ModBlock

--- src/main/java/flamepoint1544/flametech/ModBlocks.java
+++ src/main/java/flamepoint1544/flametech/ModBlocks.java
@@ -1,6 +1,25 @@
 package flamepoint1544.flametech;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.*;
+import net.minecraft.util.Identifier;
+
 public class ModBlocks {
+    public static Block register(Block block, String id, boolean registerItem){
+        // Create Identifier
+        Identifier blockID = Identifier.of(FlameTech.MOD_ID, id);
+
+        // check to see if it should register an item
+        if(registerItem){
+            BlockItem blockItem = new BlockItem(block, new Item.Settings());
+            Registry.register(Registries.ITEM, blockID, blockItem);
+        }
+
+        return Registry.register(Registries.BLOCK, id, block);
+    }
+
     public static void initialize(){
         
     }
