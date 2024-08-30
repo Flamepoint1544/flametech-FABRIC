14ae30ba1b6b62df218efb9c472a4fc03d8362da
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-08-30 13:10:36 -0600

    Added test block

--- src/main/java/flamepoint1544/flametech/ModBlocks.java
+++ src/main/java/flamepoint1544/flametech/ModBlocks.java
@@ -1,12 +1,16 @@
 package flamepoint1544.flametech;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.*;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
+    public static final Block TEST_BLOCK = register(new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.GRASS)), "test_block", true);
+
     public static Block register(Block block, String id, boolean registerItem){
         // Create Identifier
         Identifier blockID = Identifier.of(FlameTech.MOD_ID, id);
