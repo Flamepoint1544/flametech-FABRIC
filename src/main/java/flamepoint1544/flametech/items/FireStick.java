49aaddff1fb3832b33709501d85976a9c0095eb9
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-09-10 16:50:19 -0600

    added chance and removal of item upon success unless in creative

--- src/main/java/flamepoint1544/flametech/items/FireStick.java
+++ src/main/java/flamepoint1544/flametech/items/FireStick.java
@@ -1,8 +1,12 @@
 package flamepoint1544.flametech.items;
 
+import java.util.Random;
+
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -22,14 +26,30 @@ public class FireStick extends Item {
         Direction clickedFace = context.getSide();
         BlockPos firePos = positionClicked.offset(clickedFace);
         PlayerEntity playerEntity = context.getPlayer();
+        ItemStack stack = context.getPlayer().getMainHandStack();
+        ItemStack offStack = context.getPlayer().getOffHandStack();
+        Random r = new Random();
 
+        // check if client
         if(world.isClient){
             return ActionResult.SUCCESS;
         }
 
+        // Check if holding the same item in off hand and main hand
         if(playerEntity.getMainHandStack().getItem() == playerEntity.getOffHandStack().getItem()){
-            world.setBlockState(firePos, Blocks.FIRE.getDefaultState());
-            return ActionResult.SUCCESS;
+            if(r.nextInt(10) == 0){
+                // create the fire
+                world.setBlockState(firePos, Blocks.FIRE.getDefaultState());
+                world.playSound(null, firePos, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS);
+                
+                // check if creative
+                if(!playerEntity.isCreative()){
+                    // remove the items
+                    stack.decrement(1);
+                    offStack.decrement(1);
+                }
+                return ActionResult.SUCCESS;
+            }
         }
 
         return ActionResult.FAIL;
