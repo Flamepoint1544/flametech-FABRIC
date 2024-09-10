46be940cb940ce7338419ebc33e92cac2ae83a4d
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-09-10 11:42:24 -0600

    added getOutlineShape()

--- src/main/java/flamepoint1544/flametech/blocks/LogPile.java
+++ src/main/java/flamepoint1544/flametech/blocks/LogPile.java
@@ -1,12 +1,22 @@
 package flamepoint1544.flametech.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class LogPile extends Block{
 
     public LogPile(Settings settings) {
         super(settings.sounds(BlockSoundGroup.WOOD));
     }
-    
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0, 0, 0, 1, 0.8f, 1);
+    }
 }
