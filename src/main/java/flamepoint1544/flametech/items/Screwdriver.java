09a257764f307c12b80ed4c1b47f4a8537cd6e41
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-10-21 17:46:58 -0600

    added screwdriver item

---
+++ src/main/java/flamepoint1544/flametech/items/Screwdriver.java
@@ -0,0 +1,11 @@
+package flamepoint1544.flametech.items;
+
+import net.minecraft.item.Item;
+
+public class Screwdriver extends Item{
+
+    public Screwdriver(Settings settings) {
+        super(settings);
+    }
+    
+}
