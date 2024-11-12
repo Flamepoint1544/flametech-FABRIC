ba8e7e35238c282a61577f42ceeea4a36fa9c380
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-11-12 13:44:05 -0700

    added rods and tags for the tools

--- src/main/java/flamepoint1544/flametech/ModItems.java
+++ src/main/java/flamepoint1544/flametech/ModItems.java
@@ -27,6 +27,7 @@ public class ModItems {
     public static final Item FILE_COPPER = register(new File(new Item.Settings(), 25), "file_copper");
     public static final Item WRENCH_COPPER = register(new Wrench(new Item.Settings(), 25), "wrench_copper");
     public static final Item HAMMER_COPPER = register(new Hammer(new Item.Settings(), 25, BBToolMaterials.COPPER, 1, 1), "hammer_copper");
+    public static final Item ROD_COPPER = register(new Item(new Item.Settings()), "copper_rod");
 
     // Iron
     public static final Item SCREWDRIVER_IRON = register(new Screwdriver(new Item.Settings(), 50), "screwdriver_iron");
@@ -34,6 +35,7 @@ public class ModItems {
     public static final Item FILE_IRON = register(new File(new Item.Settings(), 50), "file_iron");
     public static final Item WRENCH_IRON = register(new Wrench(new Item.Settings(), 50), "wrench_iron");
     public static final Item HAMMER_IRON = register(new Hammer(new Item.Settings(), 50, ToolMaterials.IRON, 1, 1), "hammer_iron");
+    public static final Item ROD_IRON = register(new Item(new Item.Settings()), "iron_rod");
 
     // Gold
     public static final Item SCREWDRIVER_GOLD = register(new Screwdriver(new Item.Settings(), 25), "screwdriver_gold");
@@ -41,6 +43,7 @@ public class ModItems {
     public static final Item FILE_GOLD = register(new File(new Item.Settings(), 25), "file_gold");
     public static final Item WRENCH_GOLD = register(new Wrench(new Item.Settings(), 25), "wrench_gold");
     public static final Item HAMMER_GOLD = register(new Hammer(new Item.Settings(), 25, ToolMaterials.GOLD, 1, 1), "hammer_gold");
+    public static final Item ROD_GOLD = register(new Item(new Item.Settings()), "gold_rod");
 
     // Steel
 
@@ -50,6 +53,7 @@ public class ModItems {
     public static final Item FILE_DIAMOND = register(new File(new Item.Settings(), 75), "file_diamond");
     public static final Item WRENCH_DIAMOND = register(new Wrench(new Item.Settings(), 75), "wrench_diamond");
     public static final Item HAMMER_DIAMOND = register(new Hammer(new Item.Settings(), 25, ToolMaterials.DIAMOND, 1, 1), "hammer_diamond");
+    public static final Item ROD_DIAMOND = register(new Item(new Item.Settings()), "diamond_rod");
 
     // Netherite
     public static final Item SCREWDRIVER_NETHERITE = register(new Screwdriver(new Item.Settings(), 100), "screwdriver_netherite");
@@ -57,6 +61,7 @@ public class ModItems {
     public static final Item FILE_NETHERITE = register(new File(new Item.Settings(), 100), "file_netherite");
     public static final Item WRENCH_NETHERITE = register(new Wrench(new Item.Settings(), 100), "wrench_netherite");
     public static final Item HAMMER_NETHERITE = register(new Hammer(new Item.Settings(), 25, ToolMaterials.NETHERITE, 1, 1), "hammer_netherite");
+    public static final Item ROD_NETHERITE = register(new Item(new Item.Settings()), "netherite_rod");
 
     // Ultimatium
     public static final Item SCREWDRIVER_ULTIMATIUM = register(new Screwdriver(new Item.Settings(), 200), "screwdriver_ultimatium");
@@ -64,6 +69,7 @@ public class ModItems {
     public static final Item FILE_ULTIMATIUM = register(new File(new Item.Settings(), 200), "file_ultimatium");
     public static final Item WRENCH_ULTIMATIUM = register(new Wrench(new Item.Settings(), 200), "wrench_ultimatium");
     public static final Item HAMMER_ULTIMATIUM = register(new Hammer(new Item.Settings(), 25, BBToolMaterials.ULTIMATIUM, 1, 1), "hammer_ultimatium");
+    public static final Item ROD_ULTIMATIUM = register(new Item(new Item.Settings()), "ultimatium_rod");
 
     // Registry keys
     public static final RegistryKey<ItemGroup> PRIMARY_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(FlameTech.MOD_ID, "primary_item_group"));
@@ -105,30 +111,42 @@ public class ModItems {
             itemGroup.add(FILE_GOLD);
             itemGroup.add(WRENCH_GOLD);
             itemGroup.add(HAMMER_GOLD);
+            itemGroup.add(ROD_GOLD);
+
+            itemGroup.add(SCREWDRIVER_COPPER);
+            itemGroup.add(SAW_COPPER);
+            itemGroup.add(FILE_COPPER);
+            itemGroup.add(WRENCH_COPPER);
+            itemGroup.add(HAMMER_COPPER);
+            itemGroup.add(ROD_COPPER);
             
             itemGroup.add(SCREWDRIVER_IRON);
             itemGroup.add(SAW_IRON);
             itemGroup.add(FILE_IRON);
             itemGroup.add(WRENCH_IRON);
             itemGroup.add(HAMMER_IRON);
+            itemGroup.add(ROD_IRON);
 
             itemGroup.add(SCREWDRIVER_DIAMOND);
             itemGroup.add(SAW_DIAMOND);
             itemGroup.add(FILE_DIAMOND);
             itemGroup.add(WRENCH_DIAMOND);
             itemGroup.add(HAMMER_DIAMOND);
+            itemGroup.add(ROD_DIAMOND);
 
             itemGroup.add(SCREWDRIVER_NETHERITE);
             itemGroup.add(SAW_NETHERITE);
             itemGroup.add(FILE_NETHERITE);
             itemGroup.add(WRENCH_NETHERITE);
             itemGroup.add(HAMMER_NETHERITE);
+            itemGroup.add(ROD_NETHERITE);
             
             itemGroup.add(SCREWDRIVER_ULTIMATIUM);
             itemGroup.add(SAW_ULTIMATIUM);
             itemGroup.add(FILE_ULTIMATIUM);
             itemGroup.add(WRENCH_ULTIMATIUM);
             itemGroup.add(HAMMER_ULTIMATIUM);
+            itemGroup.add(ROD_ULTIMATIUM);
 
             itemGroup.add(ModBlocks.TEST_BLOCK.asItem());
             itemGroup.add(TEST_ITEM);
