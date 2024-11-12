4a918fa5ed6bfa939147d2baf9216982e58a3780
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-11-12 12:42:23 -0700

    added copper tools and resorted the items

--- src/main/java/flamepoint1544/flametech/ModItems.java
+++ src/main/java/flamepoint1544/flametech/ModItems.java
@@ -1,7 +1,8 @@
 package flamepoint1544.flametech;
 
 import flamepoint1544.flametech.items.*;
-import flamepoint1544.beyond_bedrock.ToolMaterials;
+import flamepoint1544.beyond_bedrock.BBToolMaterials;
+import net.minecraft.item.ToolMaterials;
 import net.fabricmc.fabric.api.itemgroup.v1.*;
 import net.minecraft.item.*;
 import net.minecraft.registry.*;
@@ -12,48 +13,57 @@ public class ModItems {
     // Items
     public static final Item TEST_ITEM = register(new Item(new Item.Settings()), "test_item");
     public static final Item FIRE_STICK = register(new FireStick(new Item.Settings()), "fire_stick");
-    
-    // Screwdrivers
-    public static final Item SCREWDRIVER_F = register(new Screwdriver(new Item.Settings(), 25), "screwdriver_flint");
-    public static final Item SCREWDRIVER_I = register(new Screwdriver(new Item.Settings(), 50), "screwdriver_iron");
-    public static final Item SCREWDRIVER_G = register(new Screwdriver(new Item.Settings(), 25), "screwdriver_gold");
-    public static final Item SCREWDRIVER_D = register(new Screwdriver(new Item.Settings(), 75), "screwdriver_diamond");
-    public static final Item SCREWDRIVER_N = register(new Screwdriver(new Item.Settings(), 100), "screwdriver_netherite");
-    public static final Item SCREWDRIVER_U = register(new Screwdriver(new Item.Settings(), 200), "screwdriver_ultimatium");
 
-    // Saws
-    public static final Item SAW_F = register(new Saw(net.minecraft.item.ToolMaterials.WOOD, 3.0f, -3.2f, new Item.Settings(), 25), "saw_flint");
-    public static final Item SAW_I = register(new Saw(net.minecraft.item.ToolMaterials.IRON, 3.0f, -3.2f, new Item.Settings(), 50), "saw_iron");
-    public static final Item SAW_G = register(new Saw(net.minecraft.item.ToolMaterials.GOLD, 3.0f, -3.2f, new Item.Settings(), 25), "saw_gold");
-    public static final Item SAW_D = register(new Saw(net.minecraft.item.ToolMaterials.DIAMOND, 3.0f, -3.2f, new Item.Settings(), 75), "saw_diamond");
-    public static final Item SAW_N = register(new Saw(net.minecraft.item.ToolMaterials.NETHERITE, 3.0f, -3.2f, new Item.Settings(), 100), "saw_netherite");
-    public static final Item SAW_U = register(new Saw(ToolMaterials.ULTIMATIUM, 3.0f, -3.2f, new Item.Settings(), 200), "saw_ultimatium");
+    // Flint
+    public static final Item SCREWDRIVER_FLINT = register(new Screwdriver(new Item.Settings(), 25), "screwdriver_flint");
+    public static final Item SAW_FLINT = register(new Saw(BBToolMaterials.FLINT, 3.0f, -3.2f, new Item.Settings(), 25), "saw_flint");
+    public static final Item FILE_FLINT = register(new File(new Item.Settings(), 25), "file_flint");
+    public static final Item WRENCH_FLINT = register(new Wrench(new Item.Settings(), 25), "wrench_flint");
+    public static final Item HAMMER_FLINT = register(new Hammer(new Item.Settings(), 25, ToolMaterials.WOOD, 1, 1), "hammer_flint");
 
-    // Files
-    public static final Item FILE_F = register(new File(new Item.Settings(), 25), "file_flint");
-    public static final Item FILE_I = register(new File(new Item.Settings(), 50), "file_iron");
-    public static final Item FILE_G = register(new File(new Item.Settings(), 25), "file_gold");
-    public static final Item FILE_D = register(new File(new Item.Settings(), 75), "file_diamond");
-    public static final Item FILE_N = register(new File(new Item.Settings(), 100), "file_netherite");
-    public static final Item FILE_U = register(new File(new Item.Settings(), 200), "file_ultimatium");
+    // Copper
+    public static final Item SCREWDRIVER_COPPER = register(new Screwdriver(new Item.Settings(), 25), "screwdriver_copper");
+    public static final Item SAW_COPPER = register(new Saw(BBToolMaterials.COPPER, 3.0f, -3.2f, new Item.Settings(), 25), "saw_copper");
+    public static final Item FILE_COPPER = register(new File(new Item.Settings(), 25), "file_copper");
+    public static final Item WRENCH_COPPER = register(new Wrench(new Item.Settings(), 25), "wrench_copper");
+    public static final Item HAMMER_COPPER = register(new Hammer(new Item.Settings(), 25, BBToolMaterials.COPPER, 1, 1), "hammer_copper");
 
-    // Wrenches
-    public static final Item WRENCH_F = register(new Wrench(new Item.Settings(), 25), "wrench_flint");
-    public static final Item WRENCH_C = register(new Wrench(new Item.Settings(), 25), "wrench_copper");
-    public static final Item WRENCH_I = register(new Wrench(new Item.Settings(), 50), "wrench_iron");
-    public static final Item WRENCH_G = register(new Wrench(new Item.Settings(), 25), "wrench_gold");
-    public static final Item WRENCH_D = register(new Wrench(new Item.Settings(), 75), "wrench_diamond");
-    public static final Item WRENCH_N = register(new Wrench(new Item.Settings(), 100), "wrench_netherite");
-    public static final Item WRENCH_U = register(new Wrench(new Item.Settings(), 200), "wrench_ultimatium");
+    // Iron
+    public static final Item SCREWDRIVER_IRON = register(new Screwdriver(new Item.Settings(), 50), "screwdriver_iron");
+    public static final Item SAW_IRON = register(new Saw(ToolMaterials.IRON, 3.0f, -3.2f, new Item.Settings(), 50), "saw_iron");
+    public static final Item FILE_IRON = register(new File(new Item.Settings(), 50), "file_iron");
+    public static final Item WRENCH_IRON = register(new Wrench(new Item.Settings(), 50), "wrench_iron");
+    public static final Item HAMMER_IRON = register(new Hammer(new Item.Settings(), 50, ToolMaterials.IRON, 1, 1), "hammer_iron");
 
-    // Hammer
-    public static final Item HAMMER_F = register(new Hammer(new Item.Settings(), 25, net.minecraft.item.ToolMaterials.WOOD, 1, 1), "hammer_flint");
-    public static final Item HAMMER_C = register(new Hammer(new Item.Settings(), 25, ToolMaterials.COPPER, 1, 1), "hammer_copper");
-    public static final Item HAMMER_I = register(new Hammer(new Item.Settings(), 50, net.minecraft.item.ToolMaterials.IRON, 1, 1), "hammer_iron");
-    public static final Item HAMMER_G = register(new Hammer(new Item.Settings(), 25, net.minecraft.item.ToolMaterials.GOLD, 1, 1), "hammer_gold");
-    public static final Item HAMMER_D = register(new Hammer(new Item.Settings(), 25, net.minecraft.item.ToolMaterials.DIAMOND, 1, 1), "hammer_diamond");
-    public static final Item HAMMER_N = register(new Hammer(new Item.Settings(), 25, net.minecraft.item.ToolMaterials.NETHERITE, 1, 1), "hammer_netherite");
-    public static final Item HAMMER_U = register(new Hammer(new Item.Settings(), 25, ToolMaterials.ULTIMATIUM, 1, 1), "hammer_ultimatium");
+    // Gold
+    public static final Item SCREWDRIVER_GOLD = register(new Screwdriver(new Item.Settings(), 25), "screwdriver_gold");
+    public static final Item SAW_GOLD = register(new Saw(ToolMaterials.GOLD, 3.0f, -3.2f, new Item.Settings(), 25), "saw_gold");
+    public static final Item FILE_GOLD = register(new File(new Item.Settings(), 25), "file_gold");
+    public static final Item WRENCH_GOLD = register(new Wrench(new Item.Settings(), 25), "wrench_gold");
+    public static final Item HAMMER_GOLD = register(new Hammer(new Item.Settings(), 25, ToolMaterials.GOLD, 1, 1), "hammer_gold");
+
+    // Steel
+
+    // Diamond
+    public static final Item SCREWDRIVER_DIAMOND = register(new Screwdriver(new Item.Settings(), 75), "screwdriver_diamond");
+    public static final Item SAW_DIAMOND = register(new Saw(ToolMaterials.DIAMOND, 3.0f, -3.2f, new Item.Settings(), 75), "saw_diamond");
+    public static final Item FILE_DIAMOND = register(new File(new Item.Settings(), 75), "file_diamond");
+    public static final Item WRENCH_DIAMOND = register(new Wrench(new Item.Settings(), 75), "wrench_diamond");
+    public static final Item HAMMER_DIAMOND = register(new Hammer(new Item.Settings(), 25, ToolMaterials.DIAMOND, 1, 1), "hammer_diamond");
+
+    // Netherite
+    public static final Item SCREWDRIVER_NETHERITE = register(new Screwdriver(new Item.Settings(), 100), "screwdriver_netherite");
+    public static final Item SAW_NETHERITE = register(new Saw(ToolMaterials.NETHERITE, 3.0f, -3.2f, new Item.Settings(), 100), "saw_netherite");
+    public static final Item FILE_NETHERITE = register(new File(new Item.Settings(), 100), "file_netherite");
+    public static final Item WRENCH_NETHERITE = register(new Wrench(new Item.Settings(), 100), "wrench_netherite");
+    public static final Item HAMMER_NETHERITE = register(new Hammer(new Item.Settings(), 25, ToolMaterials.NETHERITE, 1, 1), "hammer_netherite");
+
+    // Ultimatium
+    public static final Item SCREWDRIVER_ULTIMATIUM = register(new Screwdriver(new Item.Settings(), 200), "screwdriver_ultimatium");
+    public static final Item SAW_ULTIMATIUM = register(new Saw(BBToolMaterials.ULTIMATIUM, 3.0f, -3.2f, new Item.Settings(), 200), "saw_ultimatium");
+    public static final Item FILE_ULTIMATIUM = register(new File(new Item.Settings(), 200), "file_ultimatium");
+    public static final Item WRENCH_ULTIMATIUM = register(new Wrench(new Item.Settings(), 200), "wrench_ultimatium");
+    public static final Item HAMMER_ULTIMATIUM = register(new Hammer(new Item.Settings(), 25, BBToolMaterials.ULTIMATIUM, 1, 1), "hammer_ultimatium");
 
     // Registry keys
     public static final RegistryKey<ItemGroup> PRIMARY_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(FlameTech.MOD_ID, "primary_item_group"));
@@ -84,41 +94,41 @@ public class ModItems {
             itemGroup.add(ModBlocks.LOG_PILE.asItem());
 
             // Tools
-            itemGroup.add(SCREWDRIVER_F);
-            itemGroup.add(SAW_F);
-            itemGroup.add(FILE_F);
-            itemGroup.add(WRENCH_F);
-            itemGroup.add(HAMMER_F);
+            itemGroup.add(SCREWDRIVER_FLINT);
+            itemGroup.add(SAW_FLINT);
+            itemGroup.add(FILE_FLINT);
+            itemGroup.add(WRENCH_FLINT);
+            itemGroup.add(HAMMER_FLINT);
 
-            itemGroup.add(SCREWDRIVER_G);
-            itemGroup.add(SAW_G);
-            itemGroup.add(FILE_G);
-            itemGroup.add(WRENCH_G);
-            itemGroup.add(HAMMER_G);
+            itemGroup.add(SCREWDRIVER_GOLD);
+            itemGroup.add(SAW_GOLD);
+            itemGroup.add(FILE_GOLD);
+            itemGroup.add(WRENCH_GOLD);
+            itemGroup.add(HAMMER_GOLD);
             
-            itemGroup.add(SCREWDRIVER_I);
-            itemGroup.add(SAW_I);
-            itemGroup.add(FILE_I);
-            itemGroup.add(WRENCH_I);
-            itemGroup.add(HAMMER_I);
+            itemGroup.add(SCREWDRIVER_IRON);
+            itemGroup.add(SAW_IRON);
+            itemGroup.add(FILE_IRON);
+            itemGroup.add(WRENCH_IRON);
+            itemGroup.add(HAMMER_IRON);
 
-            itemGroup.add(SCREWDRIVER_D);
-            itemGroup.add(SAW_D);
-            itemGroup.add(FILE_D);
-            itemGroup.add(WRENCH_D);
-            itemGroup.add(HAMMER_D);
+            itemGroup.add(SCREWDRIVER_DIAMOND);
+            itemGroup.add(SAW_DIAMOND);
+            itemGroup.add(FILE_DIAMOND);
+            itemGroup.add(WRENCH_DIAMOND);
+            itemGroup.add(HAMMER_DIAMOND);
 
-            itemGroup.add(SCREWDRIVER_N);
-            itemGroup.add(SAW_N);
-            itemGroup.add(FILE_N);
-            itemGroup.add(WRENCH_N);
-            itemGroup.add(HAMMER_N);
+            itemGroup.add(SCREWDRIVER_NETHERITE);
+            itemGroup.add(SAW_NETHERITE);
+            itemGroup.add(FILE_NETHERITE);
+            itemGroup.add(WRENCH_NETHERITE);
+            itemGroup.add(HAMMER_NETHERITE);
             
-            itemGroup.add(SCREWDRIVER_U);
-            itemGroup.add(SAW_U);
-            itemGroup.add(FILE_U);
-            itemGroup.add(WRENCH_U);
-            itemGroup.add(HAMMER_U);
+            itemGroup.add(SCREWDRIVER_ULTIMATIUM);
+            itemGroup.add(SAW_ULTIMATIUM);
+            itemGroup.add(FILE_ULTIMATIUM);
+            itemGroup.add(WRENCH_ULTIMATIUM);
+            itemGroup.add(HAMMER_ULTIMATIUM);
 
             itemGroup.add(ModBlocks.TEST_BLOCK.asItem());
             itemGroup.add(TEST_ITEM);
