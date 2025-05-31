ca54edc31977d1d4f1d0a9f7294115c972febae2
Flamepoint1544 <Flamepoint1544@gmail.com>
2025-05-31 14:13:47 -0600

    stub item

---
+++ src/main/java/flamepoint1544/flametech/items/ModifiableItem.java
@@ -0,0 +1,9 @@
+package flamepoint1544.flametech.items;
+
+import net.minecraft.item.Item;
+
+public class ModifiableItem extends Item{
+    public ModifiableItem(Item.Settings settings){
+        super(settings);
+    }
+}
